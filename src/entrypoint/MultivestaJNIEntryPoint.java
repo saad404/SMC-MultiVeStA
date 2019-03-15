0db8d1745fd23daf0c2b5b79aa9a134672f33215
saad404 <22587048+saad404@users.noreply.github.com>
2019-03-15 15:22:23 +0100

    entrypoint edited

--- src/entrypoint/MultivestaJNIEntryPoint.java
+++ src/entrypoint/MultivestaJNIEntryPoint.java
@@ -4,6 +4,21 @@ import entrypointmultivesta.UniqueEntryPoint;
 
 public class MultivestaJNIEntryPoint {
 	public static void main(String[] args) {
-		UniqueEntryPoint.main(args);
+		if(args==null || args.length==0) {
+			String[] mvargs = {
+					"-c", 
+					"-sm", "false", 
+					"-sd", "jnisimulator.DiceJNISimulatorState",
+					"-bs", "20", 
+					"-l", "oneLocal", 
+					"-f", "stepsAfterXSteps.quatex", 
+					"-a", "0.1", 
+					"-ds", "[0.1]"
+			};
+			UniqueEntryPoint.main(mvargs);
+		}
+		else {
+			UniqueEntryPoint.main(args);
+		}
 	}
 }
