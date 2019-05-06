cba4d06412189eaad167a1bb06599f57d70124bf
Madsmal <Madsmalmskov@hotmail.com>
2019-05-06 19:31:43 +0200

    Add files via upload

---
+++ src/runserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+javac -h include jnisimulator/*.java tests/*.java -cp ../lib/multivesta.jar
+
+g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -shared -o libwebserver.so ServerSimulatorState.cpp
+
+java -cp .:../lib/multivesta.jar -Djava.library.path=. tests.MyMainServer
+
