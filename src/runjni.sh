be53870b47d31b807e13a6eec85f211456e663cb
Madsmal <Madsmalmskov@hotmail.com>
2019-06-17 22:37:07 +0200

    Delete runjni.sh

--- src/runjni.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-javac -h include jnisimulator/*.java tests/*.java -cp /home/saad404/eclipse-workspace/JNI_intro/lib/multivesta.jar 
- 
-g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -shared -o libcoinflib.so CoinFlipSimulatorState.cpp 
- 
-java -cp .:/home/saad404/eclipse-workspace/JNI_intro/lib/multivesta.jar -Djava.library.path=. tests.MyMain
